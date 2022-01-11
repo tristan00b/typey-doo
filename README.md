7ac5b20b29857587875fb6887428cff521c11deb
Tristan Bayfield <tristan.bayfield@gmail.com>
2022-01-10 18:36:56 -0800

    Add tests, docs for booleans

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## What it is?
 
-*typey-doo* is a library of utility types and helper functions for use in [Typescript](https://www.typescriptlang.org/) projects. *I do declare!*
+*typey-doo* is a library of utility types and helper functions to help with your [TypeScript](https://www.typescriptlang.org/) projects. *I do declare!*
 
 ## What it do?
 
@@ -10,7 +10,35 @@ It *doo* a lot! As much as I need it to do in my own projects... *And, probably 
 
 ## Types
 
-*TODO*
+## *Boolean Algebra &mdash; Simply bootiful!*
+
+### type: `NOT<A extends boolean> extends boolean`
+
+Negates a boolean.
+
+### type: `AND<A extends boolean, B extends boolean>`
+
+AND's two booleans.
+
+### type: `OR<A extends boolean, B extends boolean>`
+
+OR's two booleans.
+
+### type: `XOR<A extends boolean, B extends boolean>`
+
+XOR's two booleans.
+
+### type: `NAND<A extends boolean, B extends boolean> = NOT<AND<A, B>>`
+
+NAND's two booleans.
+
+### type: `NOR<A extends boolean, B extends boolean>`
+
+NOR's two booleans.
+
+### type: `XNOR<A extends boolean, B extends boolean>`
+
+XNOR's two booleans.
 
 ## Functions
 
---
+++ src/test/static/boolean.spec.ts
@@ -0,0 +1,91 @@
+/* ------------------------------------------------------------------------------------------------------------------ *\
+
+   File: boolean.spec.ts
+   Author: Tristan Bayfield
+   License: MIT
+
+\* ------------------------------------------------------------------------------------------------------------------ */
+
+import { expectType } from 'tsd'
+
+import type {
+  NOT,
+  AND,
+  OR,
+  XOR,
+  NAND,
+  NOR,
+  XNOR
+} from '../../lib/boolean'
+
+{ // NOT
+  expectType<NOT<true>>(false)
+  expectType<NOT<false>>(true)
+  expectType<NOT<true|false>>({} as boolean)
+}
+
+{ // AND
+  expectType<AND<false, false>>(false)
+  expectType<AND<false, true>>(false)
+  expectType<AND<true, false>>(false)
+  expectType<AND<true, true>>(true)
+
+  expectType<AND<boolean, true>>({} as boolean)
+  expectType<AND<boolean, false>>(false)
+  expectType<AND<boolean, boolean>>({} as boolean)
+}
+
+{ // OR
+  expectType<OR<false, false>>(false)
+  expectType<OR<false, true>>(true)
+  expectType<OR<true, false>>(true)
+  expectType<OR<true, true>>(true)
+
+  expectType<OR<boolean, true>>(true)
+  expectType<OR<boolean, false>>({} as boolean)
+  expectType<OR<boolean, boolean>>({} as boolean)
+}
+
+{ // XOR
+  expectType<XOR<false, false>>(false)
+  expectType<XOR<false, true>>(true)
+  expectType<XOR<true, false>>(true)
+  expectType<XOR<true, true >>(false)
+
+  expectType<XOR<boolean, true>>({} as boolean)
+  expectType<XOR<boolean, false>>({} as boolean)
+  expectType<XOR<boolean, boolean>>({} as boolean)
+}
+
+{ // NAND
+  expectType<NAND<false, false>>(true)
+  expectType<NAND<false, true>>(true)
+  expectType<NAND<true, false>>(true)
+  expectType<NAND<true, true>>(false)
+
+  expectType<NAND<boolean, true>>({} as boolean)
+  expectType<NAND<boolean, false>>(true)
+  expectType<NAND<boolean, boolean>>({} as boolean)
+}
+
+{ // NOR
+  expectType<NOR<false, false>>(true)
+  expectType<NOR<false, true>>(false)
+  expectType<NOR<true, false>>(false)
+  expectType<NOR<true, true>>(false)
+
+  expectType<NOR<boolean, true>>(false)
+  expectType<NOR<boolean, false>>({} as boolean)
+  expectType<NOR<boolean, boolean>>({} as boolean)
+}
+
+{ // XNOR
+  expectType<XNOR<false, false>>(true)
+  expectType<XNOR<false, true>>(false)
+  expectType<XNOR<true, false>>(false)
+  expectType<XNOR<true, true>>(true)
+
+  expectType<XNOR<boolean, true>>({} as boolean)
+  expectType<XNOR<boolean, false>>({} as boolean)
+  expectType<XNOR<boolean, boolean>>({} as boolean)
+}
