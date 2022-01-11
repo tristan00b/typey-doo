19dc04447612c70af4a3fc1d9030fa02c567c20a
Tristan Bayfield <tristan.bayfield@gmail.com>
2022-01-11 15:31:18 -0800

    Fixes, improvements

--- src/lib/tuple.d.ts
+++ src/lib/tuple.d.ts
@@ -20,10 +20,12 @@ import type {
    Tuples
 \* ------------------------------------------------------------------------------------------------------------------ */
 
-export declare type Head<T extends unknown[]> = T extends [ infer U, ...infer _ ] ? U : []
-export declare type Last<T extends unknown[]> = T extends [ ...infer _, infer U ] ? U : []
-export declare type Rest<T extends unknown[]> = T extends [ infer _, ...infer U ] ? U : []
+export declare type Concat<A extends unknown[], B extends unknown []> = [ ...A, ...B ]
+
 export declare type Drop<T extends unknown[]> = T extends [ ...infer U, infer _ ] ? U : []
+export declare type Head<T extends unknown[]> = T extends [ infer U, ...infer _ ] ? U : never
+export declare type Last<T extends unknown[]> = T extends [ ...infer _, infer U ] ? U : never
+export declare type Tail<T extends unknown[]> = T extends [ infer _, ...infer U ] ? U : []
 
 /** @todo */
 // export declare type Append<T extends unknown[], K extends unknown> = ?
@@ -35,37 +37,36 @@ export declare type Drop<T extends unknown[]> = T extends [ ...infer U, infer _ 
 // export declare type Insert<T extends unknown[], Index extends number, k extends unkonwn> = ?
 
 // aliases
-export declare type OmitFirst<T extends unknown[]> = Rest<T>
-export declare type Tail<T extends unknown[]>      = Rest<T>
+export declare type OmitFirst<T extends unknown[]> = Tail<T>
+export declare type Rest<T extends unknown[]>      = Tail<T>
 export declare type OmitLast<T extends unknown[]>  = Drop<T>
 
-export declare type FindIndex<Val, T extends unknown[], U extends unknown[]=[]> =
+declare type _FindIndex<V, T extends unknown[], U extends unknown[]=[]> =
   U['length'] extends [0, ...T]['length'] ? never
-  : Val extends T[ U['length'] ]
+  : V extends T[ U['length'] ]
       ? U['length']
-      : FindIndex<Val, T, [0, ...U]>
+      : _FindIndex<V, T, [0, ...U]>
 
-export declare type Filter<T extends unknown[], U> =
+export declare type FindIndex<V, T extends unknown[]> = _FindIndex<V, T>
+
+export declare type Filter<V, T extends unknown[]> =
   T extends [                          ] ? [                      ]
-: T extends [            U, ...infer V ] ? [      ...Filter<V, U> ]
-: T extends [ infer fst, U, ...infer V ] ? [ fst, ...Filter<V, U> ]
-: [ T[0], ...Filter<Rest<T>, U> ]
+: T extends [            V, ...infer U ] ? [      ...Filter<V, U> ]
+: T extends [ infer fst, V, ...infer U ] ? [ fst, ...Filter<V, U> ]
+: [ T[0], ...Filter<V, Tail<T>> ]
 
 export declare type Replace<T extends unknown[], U, V> =
   T extends [ infer W, ...infer Rest ] ? [ If<Equal<W, U>, V, W>, ...Replace<Rest, U, V> ] : []
 
-export declare type Concat<A extends unknown[], B extends unknown []> = [ ...A, ...B ]
-
-
 type _Repeat<
-  T,
+  V,
   N extends number,
   U extends unknown[]=[],
   I = U['length']
 > =
   I extends N
   ? []
-  : [ T, ..._Repeat<T, N, [0, ...U]> ]
+  : [ V, ..._Repeat<V, N, [0, ...U]> ]
 
 type Repeat<T, N extends number> =
   IsReal<N>     extends true ? never :
