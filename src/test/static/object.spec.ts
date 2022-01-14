40c3c8d26b4a544f5f9f216d6a09a038e606f2b5
Tristan Bayfield <tristan.bayfield@gmail.com>
2022-01-14 00:17:39 -0800

    Fix missuse of test asserts

--- src/test/static/object.spec.ts
+++ src/test/static/object.spec.ts
@@ -9,6 +9,8 @@
 import {
   expectType,
   expectNotType,
+  expectAssignable,
+  expectNotAssignable
 } from 'tsd'
 
 import type {
@@ -20,7 +22,7 @@ import type {
 } from '../../lib/object'
 
 { // AsObject
-  expectType<AsObject<[]>>([])
+  expectType<AsObject<[]>>({})
   expectType<AsObject<['a']>>({ 0: 'a' })
   expectType<AsObject<['elephant', 'giraffe']>>({ 0: 'elephant', 1: 'giraffe'})
 }
@@ -30,37 +32,32 @@ import type {
 
   const someObj = { 'a': 0, 'b': 1, c: { x: 'y' }}
 
-  expectType<KeysOf<typeof someObj>>('a')
-  expectType<KeysOf<typeof someObj>>('b')
-  expectType<KeysOf<typeof someObj>>('c')
+  expectAssignable<KeysOf<typeof someObj>>('a')
+  expectAssignable<KeysOf<typeof someObj>>('b')
+  expectAssignable<KeysOf<typeof someObj>>('c')
 
-  expectNotType<KeysOf<{ 'a': 0 }>>('moose')
+  expectNotAssignable<KeysOf<{ 'a': 0 }>>('moose')
 }
 
 { // AllKeys
   expectType<AllKeys<[]>>({} as never)
-
-  expectType<AllKeys<[{ 'a': 0 }, { 'b': 1 }, { 'elephant': 42 }]>>('a')
-  expectType<AllKeys<[{ 'a': 0 }, { 'b': 1 }, { 'elephant': 42 }]>>('b')
-  expectType<AllKeys<[{ 'a': 0 }, { 'b': 1 }, { 'elephant': 42 }]>>('elephant')
-  expectType<AllKeys<[{ 'a': 0 }, { 'a': 1 }, { 'gopher': 9000 }]>>('a')
-  expectType<AllKeys<[{ 'a': 0 }, { 'a': 1 }, { 'gopher': 9000 }]>>('gopher')
+  expectAssignable<AllKeys<[{ 'a': 0 }, { 'b': 1 }, { 'elephant': 42 }]>>('a')
+  expectAssignable<AllKeys<[{ 'a': 0 }, { 'b': 1 }, { 'elephant': 42 }]>>('b')
+  expectAssignable<AllKeys<[{ 'a': 0 }, { 'b': 1 }, { 'elephant': 42 }]>>('elephant')
 }
 
 { // ValuesOf
   expectType<ValuesOf<{}>>({} as never)
 
-  const someObj = { 'a': 0, 'b': 1, c: { x: 'y' }}
-
-  expectType<ValuesOf<typeof someObj>>(0)
-  expectType<ValuesOf<typeof someObj>>(1)
-  expectType<ValuesOf<typeof someObj>>({ x: 'y' })
+  expectAssignable<ValuesOf<{ 'a': 0, 'b': 1, c: { x: 'y' }}>>(0)
+  expectAssignable<ValuesOf<{ 'a': 0, 'b': 1, c: { x: 'y' }}>>(1)
+  expectAssignable<ValuesOf<{ 'a': 0, 'b': 1, c: { x: 'y' }}>>({ x: 'y' })
 }
 
 { // Merge
-  expectType<Merge<[]>>({} as never)
+  expectType<Merge<[]>>({})
 
-  expectType<Merge<[{ 'a': 0 }, { 'b': 1 }, { 'c': 2 }]>>({ 'a': 0, 'b': 1, 'c': 2 })
-  expectType<Merge<[{ 'a': 0 }, { 'a': 0 }]>>({ 'a': 0 })
-  expectType<Merge<[{ 'a': 0 }, { 'a': 1 }]>>({} as never)
+  expectAssignable<Merge<[{ 'a': 0 }, { 'b': 1 }, { 'c': 2 }]>>({ 'a': 0, 'b': 1, 'c': 2 })
+  expectAssignable<Merge<[{ 'a': 0 }, { 'a': 0 }]>>({ 'a': 0 })
+  expectAssignable<Merge<[{ 'a': 0 }, { 'a': 1 }]>>({} as never)
 }
