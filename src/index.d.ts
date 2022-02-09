f734b95f23df50ac255e6eec2dc405fcb0b861e9
Tristan Bayfield <tristan.bayfield@gmail.com>
2022-02-09 06:11:54 -0800

    Attempt to fix export issues

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,13 +6,13 @@
 
 \* ------------------------------------------------------------------------------------------------------------------ */
 
-export * from './lib/algebraic'
+export * from './lib/algebraic.d'
 export * from './lib/assert'
-export * from './lib/boolean'
-export * from './lib/compare'
+export * from './lib/boolean.d'
+export * from './lib/compare.d'
 export * from './lib/error'
-export * from './lib/expect'
+export * from './lib/expect.d'
 export * from './lib/nominal'
-export * from './lib/number'
-export * from './lib/object'
-export * from './lib/tuple'
+export * from './lib/number.d'
+export * from './lib/object.d'
+export * from './lib/tuple.d'
